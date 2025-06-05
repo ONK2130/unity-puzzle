f5496a452d76a2012f41c657fbb92914277d09c9
ONK2130 <a134679q@github.com>
2025-06-05 21:54:50 +0800

    Info介紹網址

---
+++ Assets/Scripts/Info_Help.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Info_Help : MonoBehaviour
+{
+    public void WEB()
+    {
+        Application.OpenURL("https://www.notion.so/Puzzl-2099b2853b5e801c80bcd750aa4145b2?pvs=4");
+    }
+
+}
